2b60739f79808c96c8411e9a6883264794a9f0d6
Milana <94888u@gmail.com>
2023-08-23 14:16:30 +0200

    whale rotation in script

--- src/whale/whale.gd
+++ src/whale/whale.gd
@@ -1,6 +1,20 @@
 extends CharacterBody2D
 
 
+@onready var physics_frames := ProjectSettings.get_setting("physics/common/physics_ticks_per_second") as float
+
+
+@export var speed := 400.0
+@export var acceleration := 40.0
+@export var friction := 60.0
+
+
+@onready var sprite := $AnimatedSprite2D
+
+
+var rotation_axis := Vector2.RIGHT
+
+
 var action_to_direction := {
 	"ui_up": Vector2.UP,
 	"ui_right": Vector2.RIGHT,
@@ -8,23 +22,48 @@ var action_to_direction := {
 	"ui_left": Vector2.LEFT,
 }
 
-var target_direction: Vector2
+var input_direction: Vector2
+
+
+func _ready() -> void:
+	sprite.play("default")
+
+
+func adjust_rotation() -> void:
+	if velocity.x < 0:
+		rotation_axis = Vector2.LEFT
+		sprite.flip_h = true
+	elif velocity.x > 0:
+		rotation_axis = Vector2.RIGHT
+		sprite.flip_h = false
+	rotation = rotation_axis.angle_to(velocity)
 
 
 func _physics_process(delta: float) -> void:
-	print(velocity)
-	velocity = target_direction * 500
+	if input_direction:
+		accelerate(input_direction.normalized(), delta * physics_frames)
+	else:
+		add_friction(delta * physics_frames)
+	adjust_rotation()
 	move_and_slide()
 
 
-func set_direction_strenght(event: InputEvent, action: StringName) -> void:
+func accelerate(direction: Vector2, delta_scale: float) -> void:
+	velocity = velocity.move_toward(direction * speed, acceleration * delta_scale)
+
+
+func add_friction(delta_scale: float) -> void:
+	velocity = velocity.move_toward(Vector2.ZERO, friction * delta_scale)
+
+
+func set_input_direction_strenght(event: InputEvent, action: StringName) -> void:
 	var direction: Vector2 = action_to_direction[action]
 	if event.is_action_pressed(action):
-		target_direction += direction
+		input_direction += direction
 	elif event.is_action_released(action):
-		target_direction -= direction
+		input_direction -= direction
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	for action in action_to_direction:
-		set_direction_strenght(event, action)
+		set_input_direction_strenght(event, action)
